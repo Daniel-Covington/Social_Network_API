ecb1f91e9324162253a658024fcf6ad40c03e69a
Daniel Covington <DanielmCovington@gmail.com>
2023-07-16 16:31:29 -0500

    Updated connection.js to be able to connect to mongodb. Created routes for thought-routes, user-routes, and created new folder controllers where i created user-controller.js and thought-controller.js

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
 
-const connectDb = async () => {
+async function connectDb() {
   try {
-    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
-      useFindAndModify: false,
+    await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017', {
       useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
+      useUnifiedTopology: true
     });
-
     console.log('Successfully connected to MongoDB');
   } catch (err) {
     console.error('Error connecting to MongoDB', err);
   }
-};
+}
 
 module.exports = { connectDb };
